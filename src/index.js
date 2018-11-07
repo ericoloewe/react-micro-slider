3bcbf97fbb0d19cda74bbc358c7b5951d5b443f4
Érico de Souza Loewe <erico.loewe@cwi.com.br>
2018-11-07 11:38:10 -0200

    feat(component): remove babel transform umd and add on change method

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@
  */
 import React from 'react';
 import MicroSlider from 'micro-slider';
+import PropTypes from 'prop-types';
 
 export default class ReactMicroSlider extends React.Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+  }
+
   componentDidMount() {
     const options = { ...this.props };
 
+    this._bind()
     delete options.children;
+    delete options.onChange;
+    options.onCycleTo = this._cycleTo
     this._microSlider = new MicroSlider(this._microSliderRef, options);
+  }
+
+  _bind() {
+    this._cycleTo = this._cycleTo.bind(this)
+  }
+
+  _cycleTo(element, draggedY) {
+    const { onChange } = this.props
+
+    if (typeof (onChange) === 'function') {
+      onChange(element, draggedY)
+    }
   }
 
   render() {
